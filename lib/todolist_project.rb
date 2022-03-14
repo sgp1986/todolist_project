9c69ce4b23b66f36f6ea5d15514b9fb70814e78f
sgp1986 <sgp1229@gmail.com>
2022-03-13 22:28:52 -0600

    updated Gemfile, added Rakefile

--- lib/todolist_project.rb
+++ lib/todolist_project.rb
@@ -1,4 +1,5 @@
 require 'bundler/setup'
+require 'stamp'
 
 # This class represents a todo item and its associated
 # data: name and description. There's also a "done"
@@ -8,7 +9,7 @@ class Todo
   DONE_MARKER = 'X'
   UNDONE_MARKER = ' '
 
-  attr_accessor :title, :description, :done
+  attr_accessor :title, :description, :done, :due_date
 
   def initialize(title, description='')
     @title = title
@@ -28,8 +29,10 @@ class Todo
     self.done = false
   end
 
-  def to_s
-    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  def to_s # replaces original #to_s method
+    result = "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+    result += due_date.stamp(' (Due: Friday January 6)') if due_date
+    result
   end
 
   def ==(otherTodo)
